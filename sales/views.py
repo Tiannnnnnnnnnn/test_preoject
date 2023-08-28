97e84dfc8fa67894e67f54e9917afeafa616ee1f
Tian <1165884660@qq.com>
2023-08-28 19:48:29 +0800

    提交

---
+++ sales/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+import os
+
+def img_return(request):
+    with open(os.getcwd() + "\sales\imgs\water.jpg", 'rb') as f:
+        img_a = f.read()
+
+    return HttpResponse(img_a, content_type = 'image/jpg')
+
+# Create your views here.
